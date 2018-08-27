332e4157e54d772a6acd7e154565e41c5b52ff5f
Matthieu GURAK <support@bourbon-digital.com>
2018-08-27 16:14:37 +0400

    we registered it as a society, physical, bank...

--- app/Entity.php
+++ app/Entity.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entity extends Model
 {
+  public function natureEntitiesId(){
+    return $this->belongsTo(NatureEntity::class);
+  }
+
   public function typeEntitiesId(){
     return $this->belongsTo(TypeEntity::class);
   }
--- database/migrations/2018_08_27_121255_create_registration_societies.php
+++ database/migrations/2018_08_27_121255_create_registration_societies.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNatureSocieties extends Migration
+class CreateRegistrationSocieties extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateNatureSocieties extends Migration
      */
     public function up()
     {
-        Schema::create('nature_entities', function (Blueprint $table) {
+        Schema::create('registration_entities', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateNatureSocieties extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('nature_entities');
+        Schema::dropIfExists('registration_entities');
     }
 }
