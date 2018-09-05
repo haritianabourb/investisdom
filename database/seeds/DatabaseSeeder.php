768ca9abaed05b2b19f791a450f88f6eabc11322
Matthieu GURAK <support@bourbon-digital.com>
2018-09-05 23:51:45 +0400

    My team has complete

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,9 +17,33 @@ class DatabaseSeeder extends Seeder
         $this->call(RegistrationEntitiesSeeder::class);
 
         // Voyager Specific
+        // Banks
+        $this->call(BanksDataTypesTableSeeder::class);
+        $this->call(BanksDataRowsTableSeeder::class);
+        $this->call(BanksPermissionsTableSeeder::class);
+        // CGP
+        $this->call(CGPDataTypesTableSeeder::class);
+        $this->call(CGPDataRowsTableSeeder::class);
+        $this->call(CGPPermissionsTableSeeder::class);
+        // Intermediaries
+        $this->call(IntermediariesDataTypesTableSeeder::class);
+        $this->call(IntermediariesDataRowsTableSeeder::class);
+        $this->call(IntermediariesPermissionsTableSeeder::class);
+        // Investors
+        $this->call(InvestorsDataTypesTableSeeder::class);
+        $this->call(InvestorsDataRowsTableSeeder::class);
+        $this->call(InvestorsPermissionsTableSeeder::class);
+        // Leaseholders
+        $this->call(LeaseholdersDataTypesTableSeeder::class);
+        $this->call(LeaseholdersDataRowsTableSeeder::class);
+        $this->call(LeaseholdersPermissionsTableSeeder::class);
         // SNC
         $this->call(SNCDataTypesTableSeeder::class);
         $this->call(SNCDataRowsTableSeeder::class);
         $this->call(SNCPermissionsTableSeeder::class);
+        // Suppliers
+        $this->call(SuppliersDataTypesTableSeeder::class);
+        $this->call(SuppliersDataRowsTableSeeder::class);
+        $this->call(SuppliersPermissionsTableSeeder::class);
     }
 }
