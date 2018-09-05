fe6431745515344e8f5074872c762ef23bd71284
Matthieu GURAK <support@bourbon-digital.com>
2018-09-05 23:32:00 +0400

    Supplier have buff and taunt

---
+++ database/seeds/SuppliersDataRowsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\DataRow;
+use TCG\Voyager\Models\DataType;
+
+class SuppliersDataRowsTableSeeder extends Seeder
+{
+    /**
+     * Auto generated seed file.
+     */
+    public function run()
+    {
+        $entititesDataType = DataType::where('slug', 'entities')->firstOrFail();
+        $sncsDataType = DataType::where('slug', 'suppliers')->firstOrFail();
+
+        foreach ($entititesDataType->rows()->get() as $row) {
+          $this
+            ->dataRow($sncsDataType, collect($row->toArray())->except(['id', 'data_type_id']))
+            ->save();
+
+        }
+    }
+
+    /**
+     * [dataRow description].
+     *
+     * @param [type] $type  [description]
+     * @param [type] $field [description]
+     *
+     * @return [type] [description]
+     */
+    protected function dataRow($type, $fields)
+    {
+        return new DataRow($fields->put('data_type_id', $type->id)->toArray());
+    }
+}
