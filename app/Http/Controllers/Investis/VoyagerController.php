0053643c363ac1612fe62b253eb2a227d21d2aca
Matthieu GURAK <support@bourbon-digital.com>
2018-09-05 23:05:12 +0400

    moving controllers

---
+++ app/Http/Controllers/Investis/VoyagerController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Investis;
+
+use TCG\Voyager\Http\Controllers\VoyagerController as BaseVoyagerController;
+
+class VoyagerController extends BaseVoyagerController
+{
+    //
+}
