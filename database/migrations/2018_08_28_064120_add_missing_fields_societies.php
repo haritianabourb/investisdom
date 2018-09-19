92a82350a649992c970f4d70ec82cff32732a6e8
Matthieu GURAK <support@bourbon-digital.com>
2018-09-20 01:01:48 +0400

    add cgp

--- database/migrations/2018_08_28_064120_add_missing_fields_societies.php
+++ database/migrations/2018_08_28_064120_add_missing_fields_societies.php
@@ -13,12 +13,23 @@ class AddMissingFieldsSocieties extends Migration
      */
     public function up()
     {
-        Schema::table('entities', function (Blueprint $table) {
+      $entities = [
+        // 'entities',
+        // 'sncs',
+        'leaseholders',
+        'investors',
+        'cgps',
+        'intermediaries',
+        'suppliers'
+      ];
+      foreach ($entities as $e) {
+        Schema::table($e, function (Blueprint $table) {
             $table->string('capital');
             $table->string('registration_city')->nullable();
             $table->string('ape_key')->nullable();
             $table->string('etablishment_code')->nullable();
         });
+      }
     }
 
     /**
@@ -28,11 +39,11 @@ class AddMissingFieldsSocieties extends Migration
      */
     public function down()
     {
-        Schema::table('entities', function (Blueprint $table) {
-            $table->dropColumn('capital');
-            $table->dropColumn('registration_city');
-            $table->dropColumn('ape_key');
-            $table->dropColumn('etablishment_code');
-        });
+        // Schema::table('entities', function (Blueprint $table) {
+        //     $table->dropColumn('capital');
+        //     $table->dropColumn('registration_city');
+        //     $table->dropColumn('ape_key');
+        //     $table->dropColumn('etablishment_code');
+        // });
     }
 }
