77a5bc3f4611333f28856aa69d7462ff11ba248d
Matthieu GURAK <support@bourbon-digital.com>
2018-09-26 01:43:33 +0400

    we can't generate conventions on adding

--- resources/views/vendor/voyager/formfields/custom/file_convention.blade.php
+++ resources/views/vendor/voyager/formfields/custom/file_convention.blade.php
@@ -1,5 +1,5 @@
 @include('voyager::formfields.file')
-@isset($dataTypeRows)
+@if($dataTypeRows == 'editRows')
 <a type="button" href="{{ route('admin.cgps.generate-convention', ['cgp' => $dataTypeContent]) }}" class="btn btn-default generate-pdf-convention col-md-4"><i class="voyager-documentation"></i> generer la convention</a>
 @endisset
 
