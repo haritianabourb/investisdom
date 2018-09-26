547152f85c89b439c88f6762aff5d0fe96af6776
Matthieu GURAK <support@bourbon-digital.com>
2018-09-26 09:43:52 +0400

    add programatic on file convention

--- resources/views/vendor/voyager/formfields/custom/file_convention.blade.php
+++ resources/views/vendor/voyager/formfields/custom/file_convention.blade.php
@@ -1,5 +1,5 @@
 @include('voyager::formfields.file')
-@if($dataTypeRows == 'editRows')
+@if(!is_null($dataTypeContent->getKey()))
 <a type="button" href="{{ route('admin.cgps.generate-convention', ['cgp' => $dataTypeContent]) }}" class="btn btn-default generate-pdf-convention col-md-4"><i class="voyager-documentation"></i> generer la convention</a>
 @endisset
 
