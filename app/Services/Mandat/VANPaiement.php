5eeaff5b186735ab06ce14ce663e34c017a59361
Matthieu GURAK <support@bourbon-digital.com>
2018-11-22 14:56:58 +0400

    adjust cash flow, match the defisc excel simulation, have same return

--- app/Services/Mandat/VANPaiement.php
+++ app/Services/Mandat/VANPaiement.php
@@ -20,18 +20,26 @@ use App\Services\AbstractField;
 		public function process(){
 			$this->period = 0;
 			$npv=0;
-			$rate = $this->parameters->get('taux_pret')/100;
+			$rate = $this->parameters->get('taux_pret');
 			$cashflows = $this->parameters->get('schedule');
 			// INVESTISSEMENT DE DEPART
-			// XXX Montant reprise materiel frounisseur ?
-			$npv += ($this->parameters->get('apport_locataire')) / pow((1 + $rate/12),  ($this->period++));
+			$npv += ($this->parameters->get('apport_locataire'));
+			// $npv += ($this->parameters->get('apport_locataire')) / pow((1 + $rate/12),  ($this->period++));
 		  foreach($cashflows as $cf) {
 				// CALCUL DU VAN POUR CHAQUE PERIODE
-		    $npv += $cf['payment'] / pow((1+$rate/12), ($this->period++));
-				// var_dump($npv);
+		    $npv += $cf['payment'] / pow((1+$rate), ($this->period++));
+				// dump($npv);
 		  }
 		  return $npv;
 		}
+
+// 		Finance.prototype.NPV = function (rate) {
+// 			var rate = rate/100, npv = arguments[1];
+// 		  for (var i = 2; i < arguments.length; i++) {
+// 		    npv +=(arguments[i] / Math.pow((1 + rate), i - 1));
+// 		  }
+// 		  return Math.round(npv * 100) / 100;
+// 		};
 
 	}
 
