f1bb1717d88a6d7408f292aaa2182980bb632c8c
Matthieu GURAK <support@bourbon-digital.com>
2018-11-22 15:01:42 +0400

    little cleanup

--- app/Services/Mandat/VANPaiement.php
+++ app/Services/Mandat/VANPaiement.php
@@ -11,11 +11,6 @@ use App\Services\AbstractField;
 	{
 
 		protected $name = "van_paiement";
-		// protected $validations = [
-		// 	"complement_financement" => "required",
-		// 	"taux_pret" => "required",
-		// 	"period" => "required"
-		// ];
 
 		public function process(){
 			$this->period = 0;
@@ -24,22 +19,12 @@ use App\Services\AbstractField;
 			$cashflows = $this->parameters->get('schedule');
 			// INVESTISSEMENT DE DEPART
 			$npv += ($this->parameters->get('apport_locataire'));
-			// $npv += ($this->parameters->get('apport_locataire')) / pow((1 + $rate/12),  ($this->period++));
 		  foreach($cashflows as $cf) {
 				// CALCUL DU VAN POUR CHAQUE PERIODE
 		    $npv += $cf['payment'] / pow((1+$rate), ($this->period++));
-				// dump($npv);
 		  }
 		  return $npv;
 		}
-
-// 		Finance.prototype.NPV = function (rate) {
-// 			var rate = rate/100, npv = arguments[1];
-// 		  for (var i = 2; i < arguments.length; i++) {
-// 		    npv +=(arguments[i] / Math.pow((1 + rate), i - 1));
-// 		  }
-// 		  return Math.round(npv * 100) / 100;
-// 		};
 
 	}
 
