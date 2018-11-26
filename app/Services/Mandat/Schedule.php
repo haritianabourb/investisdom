c9387ee00ecc0ace51c89f9a4cf557c8be4ccd5b
Matthieu GURAK <support@bourbon-digital.com>
2018-11-26 14:36:11 +0400

    reduce precision error

--- app/Services/Mandat/Schedule.php
+++ app/Services/Mandat/Schedule.php
@@ -37,22 +37,19 @@ use App\Services\AbstractField;
 		}
 
 		private function initSchedule(){
-			// TODO deport this to parameters
-			// get term periode in years
 			$this->term_years = $this->parameters->get('duree_pret')/12;
-			// $this->term_years = $parameters->nombre_periode/12;
 			$this->terms = 12;
-			// TODO show interest?
+
 			if($this->parameters->get('complement_financement') == Funding::CASH){
 				$this->period = 0;
 				$this->nbr_period = $this->terms * $this->term_years;
-				// dump($this->nbr_period, $this->parameters->get('nbr_period'), $this->parameters->get('duree_pret'));
 			}else{
 				$this->period = $this->nbr_period = $this->terms * $this->term_years;
 			}
+
 			$this->taux_pret = ($this->parameters->get('tx_pret')/100.0) / $this->terms;
 			$this->loan_amount = $this->parameters->get('loan_amount');
-
+			$this->term_pay = $this->parameters->get('term_pay');
 		}
 
 		private function calculate()
@@ -60,21 +57,18 @@ use App\Services\AbstractField;
 
 			if($this->parameters->get('complement_financement') == Funding::BANK){
 				// dd($this->taux_pret, $this->period);
-				$deno = 1.0 - 1.0 / pow((1+ $this->taux_pret),$this->period);
-				$this->term_pay = ($this->loan_amount * $this->taux_pret) / $deno;
+				// $deno = 1.0 - 1.0 / pow((1+ $this->taux_pret),$this->period);
 
+				$interest = round($this->loan_amount * $this->taux_pret, 3, PHP_ROUND_HALF_ODD);
 
-				$interest = round($this->loan_amount * $this->taux_pret, 2);
+				$this->principal = round($this->term_pay - $interest, 3, PHP_ROUND_HALF_ODD);
 
-				$this->principal = round($this->term_pay - $interest, 2);
-
-				$this->balance = round($this->loan_amount - $this->principal, 2);
+				$this->balance = round($this->loan_amount - $this->principal, 3, PHP_ROUND_HALF_ODD);
 				// dd($deno, $this->term_pay, $interest, $this->principal, $this->balance);
 			}
 
 			if($this->parameters->get('complement_financement') == Funding::CASH){
 
-				$this->term_pay = $this->loan_amount/$this->nbr_period;
 				$interest = 0;
 
 				$this->principal = round($this->term_pay, 2);
--- app/Services/Mandat/TermPay.php
+++ app/Services/Mandat/TermPay.php
@@ -20,23 +20,10 @@ use App\Services\AbstractField;
 			if($this->parameters->get('complement_financement') == Funding::BANK){
 				$deno = 1.0 - 1.0 / pow((1+ ($this->parameters->get('taux_pret'))),$this->parameters->get('period'));
 				return ($this->parameters->get('loan_amount') * ($this->parameters->get('taux_pret'))) / $deno;
-
-				// $interest = round($this->loan_amount * $this->taux_pret, 2);
-				//
-				// $this->principal = round($this->term_pay - $interest, 2);
-				//
-				// $this->balance = round($this->loan_amount - $this->principal, 2);
 			}
 
 			if($this->parameters->get('complement_financement') == Funding::CASH){
-				return $this->parameters->get('loan_amount')/$this->parameters->get('period');
-
-				// $interest = 0;
-				//
-				// $this->principal = round($this->term_pay, 2);
-				//
-				// $this->balance = round($this->loan_amount - ($this->period * $this->principal), 2);
-
+				return $this->parameters->get('loan_amount')/$this->parameters->get('duree_pret');
 			}
 
 		}
