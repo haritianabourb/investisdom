b70ed6f044a540eae81fac9122f88bff7d8027ea
Djef oO <bourbondigital974@gmail.com>
2018-12-07 12:52:28 +0400

    set the same db on dev and local

---
+++ database/seeds/12072018ContactAddressesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class 12072018ContactAddressesTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('contact_addresses')->delete();
+        
+        
+        
+    }
+}
---
+++ database/seeds/12072018PasswordResetsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class 12072018PasswordResetsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('password_resets')->delete();
+        
+        
+        
+    }
+}
