4c1ab7794a6eb8cba60e70c07b3431e0f0a591cb
Matthieu GURAK <support@bourbon-digital.com>
2018-12-11 11:17:46 +0400

    fields are coming!

--- resources/views/vendor/voyager/partials/custom/modal.blade.php
+++ resources/views/vendor/voyager/partials/custom/modal.blade.php
@@ -19,8 +19,46 @@
               <div class="modal-body">
                   {{ method_field("POST") }}
                     <!-- Adding / Editing -->
-                    {{dump($relationshipDataTypeRows)}}
+                    @foreach($relationshipDataTypeRows as $row)
+                      <!-- GET THE DISPLAY OPTIONS -->
+                      @php
+                          $options = json_decode($row->details);
+                          $display_options = isset($options->display) ? $options->display : NULL;
+                      @endphp
+                      @if ($options && isset($options->legend) && isset($options->legend->text))
+                        @if(!$loop->first)
+                          </div>
+                        @endif
+                          <div class="row">
+                            <div class="col-md-12">
+                              <legend class="text-{{$options->legend->align or 'center'}}" style="color: {{$options->legend->color or '#333'}};background-color: {{$options->legend->bgcolor or '#f0f0f0'}};padding: 5px; padding-left: 15px; display:inline-block">{{$options->legend->text}}</legend>
+                            </div>
+                      @endif
+                      <div class="form-group @if($row->type == 'hidden') hidden @endif col-md-{{ $display_options->width or 12 }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
+                      @if ($options && isset($options->formfields_custom))
+                          {{ $row->slugify }}
+                          <label for="name">{{ $row->display_name }}</label>
+                          @include('voyager::formfields.custom.' . $options->formfields_custom)
+                      @else
+                              {{ $row->slugify }}
+                              <label for="name">{{ $row->display_name }}</label>
+                              @include('voyager::multilingual.input-hidden-bread-edit-add')
+                              @if($row->type == 'relationship')
+                                  @include('voyager::formfields.relationship')
+                              @elseif (isset($options->calculate) && $options->calculate && isset($options->manual) && $options->manual)
+                                @include('voyager::formfields.custom.calculate_manual')
+                              @elseif (isset($options->calculate) && $options->calculate)
+                                @include('voyager::formfields.custom.calculate')
+                              @else
+                                  {!! app('voyager')->formField($row, $dataType, $dataTypeContent) !!}
+                              @endif
 
+                              {{-- @foreach (app('voyager')->afterFormFields($row, $dataType, $dataTypeContent) as $after)
+                                  {!! $after->handle($row, $dataType, $dataTypeContent) !!}
+                              @endforeach --}}
+                      @endif
+                    </div>
+                    @endforeach
                     <input type="submit" style="display:none;">
                   {{ csrf_field() }}
 
