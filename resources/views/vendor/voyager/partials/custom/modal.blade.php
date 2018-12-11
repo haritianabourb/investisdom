9f340619480dfa21b8b746f2a2a5f96cb92e6cf1
Matthieu GURAK <support@bourbon-digital.com>
2018-12-11 11:20:54 +0400

    make it little more pretty

--- resources/views/vendor/voyager/partials/custom/modal.blade.php
+++ resources/views/vendor/voyager/partials/custom/modal.blade.php
@@ -34,7 +34,7 @@
                               <legend class="text-{{$options->legend->align or 'center'}}" style="color: {{$options->legend->color or '#333'}};background-color: {{$options->legend->bgcolor or '#f0f0f0'}};padding: 5px; padding-left: 15px; display:inline-block">{{$options->legend->text}}</legend>
                             </div>
                       @endif
-                      <div class="form-group @if($row->type == 'hidden') hidden @endif col-md-{{ $display_options->width or 12 }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
+                      <div class="form-group @if($row->type == 'hidden') hidden @endif col-md-12" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
                       @if ($options && isset($options->formfields_custom))
                           {{ $row->slugify }}
                           <label for="name">{{ $row->display_name }}</label>
