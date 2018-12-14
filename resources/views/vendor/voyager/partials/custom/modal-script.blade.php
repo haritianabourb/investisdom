ac7b4178377e3c80653f184111ab45c5b6eb889e
Matthieu GURAK <support@bourbon-digital.com>
2018-12-14 17:33:46 +0400

    the new object can go on his place

--- resources/views/vendor/voyager/partials/custom/modal-script.blade.php
+++ resources/views/vendor/voyager/partials/custom/modal-script.blade.php
@@ -7,7 +7,7 @@
 
   $('#add_form_{{$modal_id}}').submit(function(e) {
     e.preventDefault();
-    var $elem = $('#{{$dataType->name}}_edit_add');
+    var $elem = $('#modal_{{$modal_id}}').prevUntil('div').filter('select').first();
     var formdata = $(this).serializeArray();
     var data = {};
     $(formdata).each(function(index, obj){
@@ -21,7 +21,8 @@
       success: function (response) {
         if(response.success){
           var newOption = new Option(response.data.{{$options->relationship->label}}, response.data.{{$options->relationship->key}}, true, true);
-          $('#'+$elem.prop('id')+' [name="{{$row->field}}@if(str_is("select_multiple", $row->type))[]@endif"]').append(newOption).trigger('change');
+          {{--$('#'+$elem.prop('id')+' [name="{{$row->field}}@if(str_is("select_multiple", $row->type))[]@endif"]').append(newOption).trigger('change');--}}
+          $elem.append(newOption).trigger('change');
           $('#{{$modal_id}}').modal('hide');
         }else{
           toastr.error("error on form creation, we're on!");
