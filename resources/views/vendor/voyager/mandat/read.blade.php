bbb6e1ca50c9fd06ee3c7c4bb34c9eb6c97a47f1
Matthieu GURAK <support@bourbon-digital.com>
2018-12-22 21:31:31 +0400

    update title and header of mandatory

--- resources/views/vendor/voyager/mandat/read.blade.php
+++ resources/views/vendor/voyager/mandat/read.blade.php
@@ -1,20 +1,23 @@
 @extends('voyager::master')
 
-@section('page_title', __('voyager::generic.view').' '.$dataType->display_name_singular)
+@section('page_title', $dataType->display_name_singular . ' : ' .  $dataTypeContent->identifiant)
 
 @section('page_header')
     <h1 class="page-title">
-        <i class="{{ $dataType->icon }}"></i> {{ __('voyager::generic.viewing') }} {{ ucfirst($dataType->display_name_singular) }} &nbsp;
+        <i class="{{ $dataType->icon }}"></i> {{ __('voyager::generic.viewing') }} {{ ucfirst($dataType->display_name_singular) }} : {{$dataTypeContent->identifiant}}
+        &nbsp;
 
         @can('edit', $dataTypeContent)
-        <a href="{{ route('voyager.'.$dataType->slug.'.edit', $dataTypeContent->getKey()) }}" class="btn btn-info">
-            <span class="glyphicon glyphicon-pencil"></span>&nbsp;
-            {{ __('voyager::generic.edit') }}
-        </a>
+            <a href="{{ route('voyager.'.$dataType->slug.'.edit', $dataTypeContent->getKey()) }}" class="btn btn-info">
+                <span class="glyphicon glyphicon-pencil"></span>&nbsp;
+                {{ __('voyager::generic.edit') }}
+            </a>
         @endcan
         @can('delete', $dataTypeContent)
-            <a href="javascript:;" title="{{ __('voyager::generic.delete') }}" class="btn btn-danger delete" data-id="{{ $dataTypeContent->getKey() }}" id="delete-{{ $dataTypeContent->getKey() }}">
-                <i class="voyager-trash"></i> <span class="hidden-xs hidden-sm">{{ __('voyager::generic.delete') }}</span>
+            <a href="javascript:;" title="{{ __('voyager::generic.delete') }}" class="btn btn-danger delete"
+               data-id="{{ $dataTypeContent->getKey() }}" id="delete-{{ $dataTypeContent->getKey() }}">
+                <i class="voyager-trash"></i> <span
+                        class="hidden-xs hidden-sm">{{ __('voyager::generic.delete') }}</span>
             </a>
         @endcan
 
@@ -32,34 +35,35 @@
             <div class="col-md-12">
 
                 <div class="panel panel-bordered" style="padding-bottom:5px;">
-                  <div class="panel-body" style="padding-top:10px;">
-                    <!-- form start -->
-                    @foreach($dataType->readRows as $row)
-                        @php $rowDetails = json_decode($row->details);
+                    <div class="panel-body" style="padding-top:10px;">
+                        <!-- form start -->
+                        @foreach($dataType->readRows as $row)
+                            @php $rowDetails = json_decode($row->details);
                          if($rowDetails === null){
                                 $rowDetails=new stdClass();
                                 $rowDetails->options=new stdClass();
                          }
                              $display_options = isset($rowDetails->display) ? $rowDetails->display : NULL;
-                        @endphp
-                          @if ($rowDetails && isset($rowDetails->section) && isset($rowDetails->section->text))
-                            @php
-                              $sectionOpened = true;
                             @endphp
-                            @if(!$loop->first)
-                              </div>
+                            @if ($rowDetails && isset($rowDetails->section) && isset($rowDetails->section->text))
+                                @php
+                                    $sectionOpened = true;
+                                @endphp
+                                @if(!$loop->first)
+                    </div>
+                                @endif
+                    <div class="row">
+                        <div class="col-md-12" style="border-bottom:0;">
+                            <h3 class="text-{{$rowDetails->section->align or 'center'}}"
+                                style="color: {{$rowDetails->section->color or '#333'}};background-color: {{$rowDetails->section->bgcolor or '#f0f0f0'}};padding: 5px; padding-left: 15px;">{{$rowDetails->section->text}}</h3>
+                        </div>
                             @endif
-                              <div class="row">
-                            <div class="col-md-12" style="border-bottom:0;">
-                              <h3 class="text-{{$rowDetails->section->align or 'center'}}" style="color: {{$rowDetails->section->color or '#333'}};background-color: {{$rowDetails->section->bgcolor or '#f0f0f0'}};padding: 5px; padding-left: 15px;">{{$rowDetails->section->text}}</h3>
-                            </div>
-                          @endif
-                          <div class="col-md-{{ $display_options->width or 12 }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
+                        <div class="col-md-{{ $display_options->width or 12 }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
                             <h5>{{ $row->display_name }}</h5>
                             @if($row->field == 'schedule')
-                              @include('voyager::formfields.custom.mandat_van_paiement')
-                            {{-- @elseif($row->field == 'resultats')
-                              @include('voyager::formfields.custom.mandat_resultat') --}}
+                                @include('voyager::formfields.custom.mandat_van_paiement')
+                                {{-- @elseif($row->field == 'resultats')
+                                  @include('voyager::formfields.custom.mandat_resultat') --}}
                             @elseif($row->type == "image")
                                 <img class="img-responsive"
                                      src="{{ filter_var($dataTypeContent->{$row->field}, FILTER_VALIDATE_URL) ? $dataTypeContent->{$row->field} : Voyager::image($dataTypeContent->{$row->field}) }}">
@@ -74,7 +78,7 @@
                                          src="{{ filter_var($dataTypeContent->{$row->field}, FILTER_VALIDATE_URL) ? $dataTypeContent->{$row->field} : Voyager::image($dataTypeContent->{$row->field}) }}">
                                 @endif
                             @elseif($row->type == 'relationship')
-                                 @include('voyager::formfields.relationship', ['view' => 'read', 'options' => $rowDetails])
+                                @include('voyager::formfields.relationship', ['view' => 'read', 'options' => $rowDetails])
                             @elseif($row->type == 'select_dropdown' && property_exists($rowDetails, 'options') &&
                                     !empty($rowDetails->options->{$dataTypeContent->{$row->field}})
                             )
@@ -87,15 +91,16 @@
 
                                     @foreach(json_decode($dataTypeContent->{$row->field}) as $item)
                                         @if($item->{$row->field . '_page_slug'})
-                                        <a href="{{ $item->{$row->field . '_page_slug'} }}">{{ $item->{$row->field}  }}</a>@if(!$loop->last), @endif
+                                            <a href="{{ $item->{$row->field . '_page_slug'} }}">{{ $item->{$row->field}  }}</a>@if(!$loop->last)
+                                                , @endif
                                         @else
-                                        {{ $item->{$row->field}  }}
+                                            {{ $item->{$row->field}  }}
                                         @endif
                                     @endforeach
 
                                 @elseif(property_exists($rowDetails, 'options'))
                                     @foreach(json_decode($dataTypeContent->{$row->field}) as $item)
-                                     {{ $rowDetails->options->{$item} . (!$loop->last ? ', ' : '') }}
+                                        {{ $rowDetails->options->{$item} . (!$loop->last ? ', ' : '') }}
                                     @endforeach
                                 @endif
                             @elseif($row->type == 'date' || $row->type == 'timestamp')
@@ -103,15 +108,16 @@
                             @elseif($row->type == 'checkbox')
                                 @if($rowDetails && property_exists($rowDetails, 'on') && property_exists($rowDetails, 'off'))
                                     @if($dataTypeContent->{$row->field})
-                                    <span class="label label-info">{{ $rowDetails->on }}</span>
+                                        <span class="label label-info">{{ $rowDetails->on }}</span>
                                     @else
-                                    <span class="label label-primary">{{ $rowDetails->off }}</span>
+                                        <span class="label label-primary">{{ $rowDetails->off }}</span>
                                     @endif
                                 @else
-                                {{ $dataTypeContent->{$row->field} }}
+                                    {{ $dataTypeContent->{$row->field} }}
                                 @endif
                             @elseif($row->type == 'color')
-                                <span class="badge badge-lg" style="background-color: {{ $dataTypeContent->{$row->field} }}">{{ $dataTypeContent->{$row->field} }}</span>
+                                <span class="badge badge-lg"
+                                      style="background-color: {{ $dataTypeContent->{$row->field} }}">{{ $dataTypeContent->{$row->field} }}</span>
                             @elseif($row->type == 'coordinates')
                                 @include('voyager::partials.coordinates')
                             @elseif($row->type == 'rich_text_box')
@@ -137,28 +143,30 @@
                                 @elseif(is_null($dataTypeContent->{$row->field}))
                                     <p class="label label-warning">Non renseign&eacute;</p>
                                 @else
-                                  <p>{{ $dataTypeContent->{$row->field} }}</p>
+                                    <p>{{ $dataTypeContent->{$row->field} }}</p>
                                 @endif
                             @endif
-                        @if(isset($sectionOpened) && $sectionOpened && $loop->last)
-                          </div>
-                        @endif
+                            @if(isset($sectionOpened) && $sectionOpened && $loop->last)
                         </div>
+                        @endif
+                    </div>
                     @endforeach
-                  </div><!-- panel-body -->
-
-                </div>
+                </div><!-- panel-body -->
             </div>
         </div>
+    </div>
     </div>
     {{-- Single delete modal --}}
     <div class="modal modal-danger fade" tabindex="-1" id="delete_modal" role="dialog">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('voyager::generic.close') }}"><span
+                    <button type="button" class="close" data-dismiss="modal"
+                            aria-label="{{ __('voyager::generic.close') }}"><span
                                 aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title"><i class="voyager-trash"></i> {{ __('voyager::generic.delete_question') }} {{ strtolower($dataType->display_name_singular) }}?</h4>
+                    <h4 class="modal-title"><i
+                                class="voyager-trash"></i> {{ __('voyager::generic.delete_question') }} {{ strtolower($dataType->display_name_singular) }}
+                        ?</h4>
                 </div>
                 <div class="modal-footer">
                     <form action="{{ route('voyager.'.$dataType->slug.'.index') }}" id="delete_form" method="POST">
@@ -167,7 +175,8 @@
                         <input type="submit" class="btn btn-danger pull-right delete-confirm"
                                value="{{ __('voyager::generic.delete_confirm') }} {{ strtolower($dataType->display_name_singular) }}">
                     </form>
-                    <button type="button" class="btn btn-default pull-right" data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
+                    <button type="button" class="btn btn-default pull-right"
+                            data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
                 </div>
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
@@ -176,12 +185,12 @@
 
 @section('javascript')
     @if ($isModelTranslatable)
-    <script>
-        $(document).ready(function () {
-            $('.side-body').multilingual();
-        });
-    </script>
-    <script src="{{ voyager_asset('js/multilingual.js') }}"></script>
+        <script>
+            $(document).ready(function () {
+                $('.side-body').multilingual();
+            });
+        </script>
+        <script src="{{ voyager_asset('js/multilingual.js') }}"></script>
     @endif
     <script>
         var deleteFormAction;
