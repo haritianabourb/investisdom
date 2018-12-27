bd847d4c41d640d746188156890f00477fd05bf1
hehehe yesss <artur.in.the.box@yandex.ru>
2018-12-27 16:23:11 +0300

    Added input styling for Money field on edit page

--- app/FormFields/MoneyFormField.php
+++ app/FormFields/MoneyFormField.php
@@ -6,11 +6,12 @@ class MoneyFormField extends AbstractHandler
     protected $codename = 'money';
     public function createContent($row, $dataType, $dataTypeContent, $options)
     {
-        return view('formfields.money', [
+        return view('voyager::formfields.custom.money', [
             'row' => $row,
             'options' => $options,
             'dataType' => $dataType,
             'dataTypeContent' => $dataTypeContent
         ]);
+        // return view('voyager::formfields.custom.money');
     }
 }
--- resources/views/vendor/voyager/formfields/custom/money.blade.php
+++ resources/views/vendor/voyager/formfields/custom/money.blade.php
@@ -1,9 +1,12 @@
-<input type="number"
-       class="form-control"
-       name="{{ $row->field }}"
-       data-name="{{ $row->display_name }}"
-       type="number"
-       @if($row->required == 1) required @endif
-             step="any"
-       placeholder="{{ isset($options->placeholder)? old($row->field, $options->placeholder): $row->display_name }}"
-       value="@if(isset($dataTypeContent->{$row->field})){{ old($row->field, $dataTypeContent->{$row->field}) }}@else{{old($row->field)}}@endif">
+<div class="input-group">
+	<input type="number"
+	       class="form-control"
+	       name="{{ $row->field }}"
+	       data-name="{{ $row->display_name }}"
+	       type="number"
+	       @if($row->required == 1) required @endif
+	             step="any"
+	       placeholder="{{ isset($options->placeholder)? old($row->field, $options->placeholder): $row->display_name }}"
+	       value="@if(isset($dataTypeContent->{$row->field})){{ old($row->field, $dataTypeContent->{$row->field}) }}@else{{old($row->field)}}@endif">
+  <span class="input-group-addon">€</span>
+</div>
