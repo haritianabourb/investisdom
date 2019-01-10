d38f4bb3cbb9090272141d88423f5cfe3356b5d8
hehehe yesss <artur.in.the.box@yandex.ru>
2019-01-10 16:56:58 +0300

    02_EL_CL pdf and route generated with fields, ooh

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Route::get('/00_PG', 'Investis\PGPDFController@generatePDF');
 Route::get('/01_mandat', 'Investis\MandatPDFController@generatePDF');
+Route::get('/02_el_cl', 'Investis\ELCLPDFController@generatePDF');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
