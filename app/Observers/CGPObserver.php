851032ff1b3dc8fb721bfc369b45a4b7c0bbf88c
Matthieu GURAK <support@bourbon-digital.com>
2019-01-16 15:13:07 +0400

    #103: remove unfunctionnal cgp creation process

--- app/Observers/CGPObserver.php
+++ app/Observers/CGPObserver.php
@@ -46,19 +46,19 @@ class CGPObserver
         DB::table($cgp->getTable())->where('id', $cgp->id)->update(['identifiant' => $identifiant]);
 
         // TODO contact creation cgp:
-        $contact = \App\Contact::find($cgp->contact_id);
-        $user = User::find($contact->user_id);
-        if (!$user) {
-            $role = Role::where('name', 'cgp')->firstOrFail();
-            $user = new User;
-            $user->name = $contact->full_name;
-            $user->email = $contact->email ?? "test@email.com";
-            $user->password = bcrypt('password');
-            $user->role_id = $role->id;
-            $user->save();
-
-            $contact->user_id = $user->id;
-        }
+//        $contact = \App\Contact::find($cgp->contact_id);
+//        $user = User::find($contact->user_id);
+//        if (!$user) {
+//            $role = Role::where('name', 'cgp')->firstOrFail();
+//            $user = new User;
+//            $user->name = $contact->full_name;
+//            $user->email = $contact->email ?? "test@email.com";
+//            $user->password = bcrypt('password');
+//            $user->role_id = $role->id;
+//            $user->save();
+//
+//            $contact->user_id = $user->id;
+//        }
 
         // TODO create default rate for cgp
     }
