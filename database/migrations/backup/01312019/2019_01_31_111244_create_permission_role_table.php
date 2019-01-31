01ee6e4d3f71441abe4c18f31e11e864f55f5971
Matthieu GURAK <support@bourbon-digital.com>
2019-01-31 15:18:30 +0400

    new migrations

---
+++ database/migrations/backup/01312019/2019_01_31_111244_create_permission_role_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePermissionRoleTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('permission_role', function(Blueprint $table)
+		{
+			$table->integer('permission_id')->index();
+			$table->integer('role_id')->index();
+			$table->primary(['permission_id','role_id'], 'permission_role_pkey');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('permission_role');
+	}
+
+}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,19 +19,6 @@ class UsersTableSeeder extends Seeder
         \DB::table('users')->insert(array (
             0 => 
             array (
-                'id' => 1,
-                'role_id' => 1,
-                'name' => 'support',
-                'email' => 'support@bourbon-digital.com',
-                'avatar' => 'users/August2018/KWQkYP2SxIktP9OUcmUk.jpeg',
-                'password' => '$2y$10$osF.tWlhnq3xTCiQCVFmpu314vdxwH6A7Tsqhl19MPAowVn0YRzOK',
-                'remember_token' => 'eaB9CN1DQL53ltS97S8RAlZn2Xy8ZhysDIGCeLph2p1I7xHwT1dOQejlPNhS',
-                'settings' => '{"locale":"en"}',
-                'created_at' => '2018-08-12 14:28:45',
-                'updated_at' => '2018-08-12 15:16:22',
-            ),
-            1 => 
-            array (
                 'id' => 4,
                 'role_id' => 1,
                 'name' => 'Artur',
@@ -43,7 +30,7 @@ class UsersTableSeeder extends Seeder
                 'created_at' => '2018-10-02 05:20:07',
                 'updated_at' => '2018-10-02 05:20:07',
             ),
-            2 => 
+            1 => 
             array (
                 'id' => 2,
                 'role_id' => 1,
@@ -56,7 +43,7 @@ class UsersTableSeeder extends Seeder
                 'created_at' => '2018-08-13 13:25:50',
                 'updated_at' => '2018-08-13 13:25:50',
             ),
-            3 => 
+            2 => 
             array (
                 'id' => 3,
                 'role_id' => 3,
@@ -69,18 +56,31 @@ class UsersTableSeeder extends Seeder
                 'created_at' => '2018-08-23 17:14:58',
                 'updated_at' => '2018-08-23 17:14:58',
             ),
-            4 => 
+            3 => 
             array (
                 'id' => 5,
                 'role_id' => 4,
                 'name' => 'Contact1 TEST1',
                 'email' => 'test@email.com',
                 'avatar' => 'users/default.png',
-                'password' => '$2y$10$IKGP2eKXA4vzltvQFJwdyuQ6DEasK3y4XlIoRNh8JoAqmdI/UoGam',
+                'password' => '$2y$10$D5MGFwzP.AYXtTCQJVCsw.7Ws47SwTtAMiJN24ZjGn5MGLWQ74Z4e',
                 'remember_token' => NULL,
-                'settings' => NULL,
+                'settings' => '{"locale":"en"}',
                 'created_at' => '2019-01-11 10:55:43',
-                'updated_at' => '2019-01-11 10:55:43',
+                'updated_at' => '2019-01-30 08:00:15',
+            ),
+            4 => 
+            array (
+                'id' => 1,
+                'role_id' => 1,
+                'name' => 'support',
+                'email' => 'support@bourbon-digital.com',
+                'avatar' => 'users/August2018/KWQkYP2SxIktP9OUcmUk.jpeg',
+                'password' => '$2y$10$osF.tWlhnq3xTCiQCVFmpu314vdxwH6A7Tsqhl19MPAowVn0YRzOK',
+                'remember_token' => 'zrng7JRVEevFscPQ80vmnIhkOZxELvsPtbdBCbdQFgU8a3Ks1kyS8n5F1noP',
+                'settings' => '{"locale":"en"}',
+                'created_at' => '2018-08-12 14:28:45',
+                'updated_at' => '2018-08-12 15:16:22',
             ),
         ));
         
