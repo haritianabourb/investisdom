a0c1eda7d4b161f5726f40573902a5bd54ac0b6b
Matthieu GURAK <support@bourbon-digital.com>
2019-02-21 10:26:47 +0400

    !32#note_3930 : removed *100 on money field view

--- resources/views/vendor/voyager/partials/money.blade.php
+++ resources/views/vendor/voyager/partials/money.blade.php
@@ -3,9 +3,9 @@
   $field_name=$row->field;
 
   if(\Illuminate\Support\Str::endsWith(\Request::route()->getName(), 'index')){
-    $money_value=floatval($data->$field_name)*100;
+    $money_value=floatval($data->$field_name);
   }else{
-    $money_value=floatval($dataTypeContent[$field_name])*100;
+    $money_value=floatval($dataTypeContent[$field_name]);
   }
 
 @endphp
