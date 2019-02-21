c107ec74f6f19bd577858208398de4a8bb0f79f3
Matthieu GURAK <support@bourbon-digital.com>
2019-02-21 11:46:30 +0400

    !32#note_3976: buttons works

--- resources/views/vendor/voyager/dimmer.blade.php
+++ resources/views/vendor/voyager/dimmer.blade.php
@@ -1,10 +1,10 @@
 <div class="panel widget center bgimage" style="margin-bottom:0;overflow:hidden;background-image:url('{{ $image }}');">
     <div class="dimmer"></div>
     <div class="panel-content">
-
         @if (isset($icon))<i class='{{ $icon }}'></i>@endif
         <h4>{!! $title !!}</h4>
         {!! $text !!}
+            <br/>
+        <a href="{{ $button['link'] }}" class="btn btn-primary">{!! $button['text'] !!}</a>
     </div>
-    <a href="{{ $button['link'] }}" class="btn btn-primary">{!! $button['text'] !!}</a>
 </div>
