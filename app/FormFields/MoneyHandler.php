36a83924df8a87a6bd0408362ee7bc797ffe57a1
Matthieu GURAK <support@bourbon-digital.com>
2019-03-12 11:16:35 +0400

    change form fields handler name

--- app/FormFields/MoneyHandler.php
+++ app/FormFields/MoneyHandler.php
@@ -1,12 +1,12 @@
 <?php
 namespace App\FormFields;
 use TCG\Voyager\FormFields\AbstractHandler;
-class PercentageFormField extends AbstractHandler
+class MoneyHandler extends AbstractHandler
 {
-    protected $codename = 'percentage';
+    protected $codename = 'money';
     public function createContent($row, $dataType, $dataTypeContent, $options)
     {
-        return view('voyager::formfields.percentage', compact('row', 'options', 'dataType', 'dataTypeContent'));
+        return view('voyager::formfields.custom.money', compact('row', 'options', 'dataType', 'dataTypeContent'));
         // return view('voyager::formfields.custom.money');
     }
 }
