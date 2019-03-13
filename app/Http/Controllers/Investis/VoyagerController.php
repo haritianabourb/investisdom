7a33a5e1d3667b8d1b63e0c407c48b2a8dd4aa57
Matthieu GURAK <support@bourbon-digital.com>
2019-03-13 11:46:22 +0400

    use logging

--- app/Http/Controllers/Investis/VoyagerController.php
+++ app/Http/Controllers/Investis/VoyagerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Investis;
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use TCG\Voyager\Http\Controllers\VoyagerController as BaseVoyagerController;
 
 class VoyagerController extends BaseVoyagerController
@@ -11,9 +12,12 @@ class VoyagerController extends BaseVoyagerController
 
     public function assets($path)
     {
+        Log::info('Path Before', [$path]);
         $path = str_start(str_replace(['../', './'], '', $path), '/');
 //        $path = __DIR__.'/../../../publishable/assets'.$path;
         $path = base_path('vendor/tcg/voyager/publishable/assets').$path;
+        Log::info('Path After', [$path]);
+        Log::info('See if File exist: ', [File::exists($path)]);
         if (File::exists($path)) {
             $mime = '';
             if (ends_with($path, '.js')) {
@@ -27,10 +31,11 @@ class VoyagerController extends BaseVoyagerController
             $response->setSharedMaxAge(31536000);
             $response->setMaxAge(31536000);
             $response->setExpires(new \DateTime('+1 year'));
-
+            Log::info("yes it exist", [$response]);
             return $response;
         }
 
+        Log::error("no doesn't exist", [$path]);
         return response('', 404);
     }
 }
