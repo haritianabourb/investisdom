4ad4c743a1799b9447b97843a9c3b6e8c1198f28
Matthieu GURAK <support@bourbon-digital.com>
2019-03-18 15:15:01 +0400

    update migrations

--- database/seeds/user/seeds/UsersTableSeeder.php
+++ database/seeds/user/seeds/UsersTableSeeder.php
@@ -19,19 +19,6 @@ class UsersTableSeeder extends Seeder
         \DB::table('users')->insert(array (
 
             array (
-                'id' => 4,
-                'role_id' => 1,
-                'name' => 'Artur',
-                'email' => 'artur@bourbon-digital.com',
-                'avatar' => 'users/default.png',
-                'password' => '$2y$10$C2KfpVIZNOYIfttS1J0aeuKD1rA0.OUnaVl1lUuBkvlNFDZvlwNJS',
-                'remember_token' => NULL,
-                'settings' => '{"locale":"en"}',
-                'created_at' => '2018-10-02 05:20:07',
-                'updated_at' => '2018-10-02 05:20:07',
-            ),
-
-            array (
                 'id' => 2,
                 'role_id' => 1,
                 'name' => 'djefoo',
@@ -51,23 +38,10 @@ class UsersTableSeeder extends Seeder
                 'email' => 'c.monel@investis-dom.com',
                 'avatar' => 'users/August2018/I9E3dxrV7J5OEUAzqFK2.png',
                 'password' => '$2y$10$kQTnOvxsdVaq4KeB6u6vquorkMEwv8yJlelP8gQu4YwZxwOL6I50m',
-                'remember_token' => 'hdog5o46F8RxoSNcHg368s87JyLLZtYzNA084e4v2wsLAGTbzVbXy8Pr0sv8',
+                'remember_token' => null,
                 'settings' => '{"locale":"fr"}',
                 'created_at' => '2018-08-23 17:14:58',
                 'updated_at' => '2018-08-23 17:14:58',
-            ),
-
-            array (
-                'id' => 5,
-                'role_id' => 4,
-                'name' => 'DURAND MARC',
-                'email' => 'rivard.johan@email.com',
-                'avatar' => 'users/default.png',
-                'password' => '$2y$10$VjDInBl7SlJ08ltaetJjdeKG3uJ44oX2kjjPmzPLEDmGU0vR9OIzq',
-                'remember_token' => NULL,
-                'settings' => NULL,
-                'created_at' => '2019-03-14 05:22:33',
-                'updated_at' => '2019-03-14 05:22:36',
             ),
 
             array (
@@ -77,50 +51,12 @@ class UsersTableSeeder extends Seeder
                 'email' => 'support@bourbon-digital.com',
                 'avatar' => 'users/August2018/KWQkYP2SxIktP9OUcmUk.jpeg',
                 'password' => '$2y$10$X9M..b2stIB0Xabmtbr5s.vQMEx0Ks58ypam1B9XH27Pwzx2rSbya',
-                'remember_token' => 'phATRzivlguhGRjtu2ra5QjnNPry8fvtj6DiEgcFvID0DSYz8RBdlrNXAEZT',
+                'remember_token' => null,
                 'settings' => '{"locale":"fr"}',
                 'created_at' => '2018-08-12 14:28:45',
                 'updated_at' => '2019-03-14 05:38:33',
             ),
 
-            array (
-                'id' => 6,
-                'role_id' => 4,
-                'name' => 'ASSIST MéLODIE',
-                'email' => 'assist-melodir@test.email',
-                'avatar' => 'users/default.png',
-                'password' => '$2y$10$Iqb3/LEdM.rBfsmjjr3hm.GluKoXs0AiCW2WyH0trQLKbWjMvEH/y',
-                'remember_token' => NULL,
-                'settings' => NULL,
-                'created_at' => '2019-03-14 06:28:26',
-                'updated_at' => '2019-03-14 06:59:42',
-            ),
-
-            array (
-                'id' => 8,
-                'role_id' => 4,
-                'name' => 'Test MODAL ON CGP',
-                'email' => 'support@test.email',
-                'avatar' => 'users/default.png',
-                'password' => '$2y$10$kU4.lSxcFOvd9Tu0M9PwxOe1gXk/47rWtI70xOqtvolkvGoNpToQu',
-                'remember_token' => NULL,
-                'settings' => NULL,
-                'created_at' => '2019-03-14 09:19:08',
-                'updated_at' => '2019-03-14 09:19:12',
-            ),
-
-            array (
-                'id' => 9,
-                'role_id' => 4,
-                'name' => 'Consseily ALY',
-                'email' => 'conseil.aly@email.com',
-                'avatar' => 'users/default.png',
-                'password' => '$2y$10$Pyw0s6JWnoPGfadVKBiwIuqQvXsedkenhgX58/UujTOflwTJnxuny',
-                'remember_token' => NULL,
-                'settings' => NULL,
-                'created_at' => '2019-03-14 09:20:41',
-                'updated_at' => '2019-03-14 09:20:44',
-            ),
         ));
 
 
