6c87633aed45ad49ebcde69f01598f2d10ced7b9
Matthieu GURAK <support@bourbon-digital.com>
2019-03-18 12:52:43 +0400

    reservation brwose view 'identifiant' format

---
+++ resources/views/vendor/voyager/reservations/partials/identifiant-browse.blade.php
@@ -0,0 +1,11 @@
+<a href="{{ route('voyager.'.$dataType->slug.'.show', [$dataType->name => $reservation]) }}">
+{{$reservation->identifiant}}
+</a>
+<br>
+{{$reservation->investor->full_name}}<br>
+<span class="label label-primary">
+    {{$reservation->typeContratsId->nom}}
+</span><br><br>
+<span>SNC Souscrit @if($reservation->nbr_snc > 1)s @endif : {{$reservation->nbr_snc}}</span><br>
+<span>Ass Jud. : {{ $reservation->type_aj ? __('generic.yes') : __('generic.no') }}</span><br>
+<span>{{ucfirst($reservation->mode_paiement)}}</span><br>
