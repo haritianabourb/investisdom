92aa3532671655a61d0acdfde48c744584750253
Matthieu GURAK <support@bourbon-digital.com>
2019-03-20 12:07:52 +0400

    labeling as good

--- resources/views/vendor/voyager/formfields/text.blade.php
+++ resources/views/vendor/voyager/formfields/text.blade.php
@@ -1,4 +1,4 @@
-<input @if($row->required == 1) required @endif type="text" class="form-control" name="{{ $row->field }}"
+<input @if($row->required == 1) required @endif type="text" class="form-control" name="{{ $row->field }}" id="{{ $row->field }}"
         placeholder="{{ isset($options->placeholder)? old($row->field, $options->placeholder): $row->display_name }}"
        {!! isBreadSlugAutoGenerator($options) !!}
        value="{{ $dataTypeContent->{$row->field} ?? old($row->field) ?? $options->default ?? '' }}">
